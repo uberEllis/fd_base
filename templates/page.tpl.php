ee8c2a4be4605bae49ab8fc9a2c0b456852c126b
uberEllis <andre.ellis@creativespark.co.za>
2015-06-01 15:37:03 +0200

    Ready for the presentation screenshots. About to move back to a proper, useable base

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,62 +1,32 @@
-<section id="content" data-equalizer data-equalizer-mq="large-up">
+<section id="content" data-equalizer data-equalizer-mq="medium-up">
   
-  <section role="main" id="pageContent" class="" data-equalizer-watch><?php // columns large-7 large-push-2 ?>
+  <section role="main" id="pageContent" class="pageContent" data-equalizer-watch><?php // columns large-7 large-push-2 ?>
     <h1 class="page-title">Main content</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Drumstick cupim filet mignon meatball, bacon t-bone salami pig kielbasa biltong fatback landjaeger pork chop sirloin frankfurter. Sausage kielbasa short loin shankle, chuck drumstick tongue swine flank andouille pork loin shoulder short ribs pancetta. Cupim ham hock porchetta meatloaf ground round strip steak alcatra capicola turducken boudin venison beef ribs pork belly chicken. Landjaeger ham hock tri-tip pig fatback t-bone. Cow pig strip steak jowl bacon tail.</p>
   </section>
 
+  <?php if ($page['sidebar_first'] || $page['sidebar_second']) { ?>
   <aside id="sidebars">
     <?php if ($page['sidebar_first']) { ?>
-    <section id="sidebar-first" class="" data-equalizer-watch><?php // columns large-2 large-pull-7 ?>
+    <section id="sidebar-first" class="sidebar-first" data-equalizer-watch><?php // columns large-2 large-pull-7 ?>
       <h2>Left sidebar title</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Ball tip corned beef alcatra, chuck tongue chicken swine pork chop. Spare ribs fatback doner meatball chuck prosciutto ground round drumstick, picanha porchetta shoulder strip steak boudin pig. Shank chicken tenderloin meatloaf beef, fatback pig bacon pork loin. Salami landjaeger leberkas porchetta. Short loin ham biltong bresaola. Shank cupim bresaola t-bone beef shoulder doner jerky tail pastrami landjaeger.</p>
     </section><!-- #sidebar-second -->
     <?php } ?>
 
     <?php if ($page['sidebar_second']) { //|| $page['CTA']) { ?>
-    <section id="sidebar-second" class="" data-equalizer-watch><?php // columns large-3 ?>
+    <section id="sidebar-second" class="sidebar-second" data-equalizer-watch><?php // columns large-3 ?>
       <h2>Right sidebar title</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Shankle boudin cupim, porchetta bresaola salami ham. Jowl ground round flank turkey. Cupim pork belly drumstick tri-tip jerky short loin pig landjaeger alcatra. Tail meatloaf pig ground round pork belly corned beef tongue leberkas alcatra porchetta turducken. Pork chop shankle beef ribs, sausage ground round pastrami capicola tri-tip meatloaf. Shank ball tip ground round cupim tenderloin, chuck andouille picanha hamburger pork beef ribs turkey tri-tip ham.</p>
     </section><!-- #sidebar-second -->
     <?php } ?>
   </aside><!-- #sidebars -->
+  <?php } // endif sider or sidebar ?>
 </section>
-  
+
+
 <?php // Footer ?>
 <section id="pagefooter">
-  <?php // Polyptych ?>
-  <?php if (($page['polyptych_first']) || ($page['polyptych_01']) || ($page['polyptych_02']) || ($page['polyptych_last'])) { ?>
-  <div class="row show-for-medium-up">
-    <section id="polyptych">
-      <div id="polyptych-first" class="medium-6 large-3 column">
-        <?php if ($page['polyptych_first']) { print render($page['polyptych_first']); } else { print '&nbsp;';} ?>
-      </div>
-      <div id="polyptych-01" class="medium-6 large-3 column">
-        <?php if ($page['polyptych_01']) { print render($page['polyptych_01']); } else { print '&nbsp;';} ?>
-      </div>
-      <div id="polyptych-02" class="medium-6 large-3 column">
-        <?php if ($page['polyptych_02']) { print render($page['polyptych_02']); } else { print '&nbsp;';} ?>
-      </div>
-      <div id="polyptych-last" class="medium-6 large-3 column">
-        <?php if ($page['polyptych_last']) { print render($page['polyptych_last']); } else { print '&nbsp;';} ?>
-      </div>
-    
-    </section>
-  </div>
-  <?php } ?>
-  <?php // end Polyptych ?>
-
-  <?php // UX Menu ?>
-  <?php if ($page['uxmenu']) { ?>
-  <div class="row show-for-medium-up">
-    <div class="small-12 columns">
-      <div id="uxmenu">
-        <?php print render($page['uxmenu']); ?>
-      </div>
-    </div>
-  </div><!-- row -->
-  <?php // end UX Menu ?>
-  <?php } ?>
 
   <?php if ($page['footer_message'] || $page['colophon']) { ?>
   <div class="row">
